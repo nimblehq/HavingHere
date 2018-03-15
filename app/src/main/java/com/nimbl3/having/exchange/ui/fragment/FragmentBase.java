53fecd7878bd403f104188b9f848625398535316
thuypt <cuasodayleo@gmail.com>
2018-03-15 15:09:15 +0700

    Init project with tab and theme

---
+++ app/src/main/java/com/nimbl3/having/exchange/ui/fragment/FragmentBase.java
@@ -0,0 +1,10 @@
+package com.nimbl3.having.exchange.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by thuypham on 3/15/2018 AD.
+ */
+
+public class FragmentBase extends Fragment {
+}
---
+++ app/src/main/java/com/nimbl3/having/exchange/ui/fragment/FragmentListDemand.java
@@ -0,0 +1,11 @@
+package com.nimbl3.having.exchange.ui.fragment;
+
+/**
+ * Created by thuypham on 3/15/2018 AD.
+ */
+
+public class FragmentListDemand extends FragmentBase {
+    public static FragmentListDemand newInstance() {
+        return null;
+    }
+}
